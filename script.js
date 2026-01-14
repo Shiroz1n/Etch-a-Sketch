4c5b66b2b8f9040f759802d79e7544c97b063a6d
Alan Inhuma <aspas_inhuma321@outlook.com>
2026-01-14 10:56:34 -0300

    Criado todas a div por DOM no JS Inicio do CSS organizando em grid por flexbox as div

---
+++ script.js
@@ -0,0 +1,66 @@
+const container = document.querySelector("#container");
+
+const div1 = document.createElement("div");
+div1.classList.add("div1");
+
+const div2 = document.createElement("div");
+div2.classList.add("div2");
+
+const div3 = document.createElement("div");
+div3.classList.add("div3");
+
+const div4 = document.createElement("div");
+div4.classList.add("div4");
+
+const div5 = document.createElement("div");
+div5.classList.add("div5");
+
+const div6 = document.createElement("div");
+div6.classList.add("div6");
+
+const div7 = document.createElement("div");
+div7.classList.add("div7");
+
+const div8 = document.createElement("div");
+div8.classList.add("div8");
+
+const div9 = document.createElement("div");
+div9.classList.add("div9");
+
+const div10 = document.createElement("div");
+div10.classList.add("div10");
+
+const div11 = document.createElement("div");
+div11.classList.add("div11");
+
+const div12 = document.createElement("div");
+div12.classList.add("div12");
+
+const div13 = document.createElement("div");
+div13.classList.add("div13");
+
+const div14 = document.createElement("div");
+div14.classList.add("div14");
+
+const div15 = document.createElement("div");
+div15.classList.add("div15");
+
+const div16 = document.createElement("div");
+div16.classList.add("div16");
+
+container.appendChild(div1);
+container.appendChild(div2);
+container.appendChild(div3);
+container.appendChild(div4);
+container.appendChild(div5);
+container.appendChild(div6);
+container.appendChild(div7);
+container.appendChild(div8);
+container.appendChild(div9);
+container.appendChild(div10);
+container.appendChild(div11);
+container.appendChild(div12);
+container.appendChild(div13);
+container.appendChild(div14);
+container.appendChild(div15);
+container.appendChild(div16);
