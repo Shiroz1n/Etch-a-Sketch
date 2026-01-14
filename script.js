ff198b98b2cd7d450a49543494bf1ed55c5c4b17
Alan Inhuma <aspas_inhuma321@outlook.com>
2026-01-14 19:16:38 -0300

    Agora as divs pintam de forma aleatoria.

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const answer = prompt("Quantos blocos de desenho você quer?");
 
+const randomizeRGB = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+}
+
 const container = document.querySelector("#container");
 
 for (let i = 0; i < answer; i++) {
+    const corAtual = randomizeRGB();
     const div = document.createElement("div");
     div.classList.add("quadrado")
 
     div.addEventListener("mouseover", function(e) { // mouseover significa:avise quando o mouse entrar nesse elemento
-        e.target.style.backgroundColor = "black";
+        console.log("Tentando pintar com:", randomizeRGB());
+        e.target.style.backgroundColor = corAtual;
     })
 
     container.appendChild(div);
